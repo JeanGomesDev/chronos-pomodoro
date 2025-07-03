ad1c8b7881e4b36d0fe0efabcec32ff931564834
JeanGomesDev <jeangomes.dev@outlook.com>
2025-07-03 15:16:09 +0100

    preparing environment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/theme.css';
+import './styles/global.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Chronos Pomodoro</h1>
+            <p>Your productivity companion.</p>
+        </div>
+    )
+}
+
+export default App;
