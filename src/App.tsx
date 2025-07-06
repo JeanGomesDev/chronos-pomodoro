8bc4c9d862d78472f6fcffbc3224454670e0149d
JeanGomesDev <jeangomes.dev@outlook.com>
2025-07-06 17:25:09 +0100

    feat: starting to create the component with the name Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container/index';
-import { Heading } from './components/Heading/index';
+import { Logo } from './components/Logo';
 
 
 function App() {
     return (
         <>
             <Container>
-                <Heading>Welcome to My App</Heading>
+                <Logo />
             </Container>
         </>
     )
