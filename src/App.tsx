88dc28a52bbcf71e59daeaecf435e746c9a24208
JeanGomesDev <jeangomes.dev@outlook.com>
2025-07-07 16:19:46 +0100

    feat: starting create component DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container/index';
 import { Logo } from './components/Logo';
+import { CountDown } from './components/CountDown';
 import { Menu } from './components/Menu';
+import { DefaultInput } from './components/DefaultInput';
 
 
 function App() {
@@ -14,6 +16,31 @@ function App() {
 
             <Container>
                 <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <form className="form" action="">
+                    <div className="formRow">
+                        <DefaultInput id="input" type='text' />
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
