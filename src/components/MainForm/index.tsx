b38745ab03b66f83a70fa9ca640d4e10306e6a4a
JeanGomesDev <jeangomes.dev@outlook.com>
2025-07-18 19:22:55 +0100

    feat:creating the logic of time for work and break

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,12 +5,17 @@ import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContent/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 
 
 export function MainForm() {
-    const { setState } = useTaskContext();
+    const { state, setState } = useTaskContext();
 
     const taskNameInput = useRef<HTMLInputElement>(null);
+
+    const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle);
 
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -31,7 +36,7 @@ export function MainForm() {
             completeDate: null,
             interruptDate: null,
             duration: 1,
-            type: 'workTime'
+            type: nextCycleType,
         }
 
         const secondsRemaining = newTask.duration * 60;
@@ -43,7 +48,7 @@ export function MainForm() {
                     ...prevState.config
                 },
                 activeTask: newTask,
-                currentCycle: 1,
+                currentCycle: nextCycle,
                 secondsRemaining,
                 formattedSecondsRemaining: '00:00',
                 tasks: [...prevState.tasks, newTask],
