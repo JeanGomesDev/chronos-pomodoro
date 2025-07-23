99ca7544a0b915fc578cc8f582b1a7668bcf06e9
JeanGomesDev <jeangomes.dev@outlook.com>
2025-07-23 16:20:27 +0100

    feat: configured so far to use the Reducer

--- src/contexts/TaskContent/taskReducer.ts
+++ src/contexts/TaskContent/taskReducer.ts
@@ -20,7 +20,18 @@ export function taskReducer(state: TaskStateModel, action: TaskActionModal): Tas
       };
     }
     case TaskActionTypes.INTERRUPT_TASK: {
-      return state;
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: "00:00",
+        tasks: state.tasks.map((task) => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
+      };
     }
     case TaskActionTypes.RESET_STATE: {
       return state;
