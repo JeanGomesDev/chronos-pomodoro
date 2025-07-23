213308f9ad60fcd29d9d3105088365e495f6ffd0
JeanGomesDev <jeangomes.dev@outlook.com>
2025-07-23 16:09:04 +0100

    feat: starting to configure the reducer to receive my types

--- src/contexts/TaskContent/TaskContext.tsx
+++ src/contexts/TaskContent/TaskContext.tsx
@@ -1,15 +1,16 @@
 import { createContext } from "react";
 import { initialTaskState } from "./initialTaskState";
 import type { TaskStateModel } from "../../models/TaskStateModel";
+import type { TaskActionModal } from "./taskActions";
 
 type TaskContextProps = {
   state: TaskStateModel;
-  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+  dispatch: React.Dispatch<TaskActionModal>;
 };
 
 const initialContextValue = {
   state: initialTaskState,
-  setState: () => {},
+  dispatch: () => {},
 };
 
 export const TaskContext = createContext<TaskContextProps>(initialContextValue);
---
+++ src/contexts/TaskContent/taskReducer.ts
@@ -0,0 +1,31 @@
+import type { TaskStateModel } from "../../models/TaskStateModel";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { TaskActionTypes, type TaskActionModal } from "./taskActions";
+
+export function taskReducer(state: TaskStateModel, action: TaskActionModal): TaskStateModel {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      const newTask = action.payload;
+      const nextCycle = getNextCycle(state.currentCycle);
+      const secondsRemaining = newTask.duration * 60;
+
+      return {
+        ...state,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...state.tasks, newTask],
+      };
+    }
+    case TaskActionTypes.INTERRUPT_TASK: {
+      return state;
+    }
+    case TaskActionTypes.RESET_STATE: {
+      return state;
+    }
+  }
+
+  return state;
+}
