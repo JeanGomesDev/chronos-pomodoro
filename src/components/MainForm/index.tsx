90289f83e185134838d9678a5e9e17f1777eb37c
JeanGomesDev <jeangomes.dev@outlook.com>
2025-10-31 18:10:34 +0000

    feat: att some components to show your tasks activity

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -14,8 +14,8 @@ import { showMessage } from "../../adapters/showMessage";
 
 export function MainForm() {
     const { state, dispatch } = useTaskContext();
-
     const taskNameInput = useRef<HTMLInputElement>(null);
+    const lastTaskName = state.tasks[state.tasks.length - 1]?.name || '';
 
     const nextCycle = getNextCycle(state.currentCycle);
     const nextCycleType = getNextCycleType(nextCycle);
@@ -65,6 +65,7 @@ export function MainForm() {
                     placeholder="Type something"
                     ref={taskNameInput}
                     disabled={!!state.activeTask}
+                    defaultValue={lastTaskName}
                 />
             </div>
 
